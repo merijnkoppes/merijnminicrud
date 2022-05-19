3d15f883a4a45c5472eea8d35e24f4c1dc5381db
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2022-05-19 13:29:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Restaurant</title>
+  <title>Ria's</title>
   <link rel="stylesheet" href="css/style.css">
   <link href='https://fonts.googleapis.com/css?family=Josefin Sans' rel='stylesheet'>
 </head>
