a8322c3d60c08a1af54be7357db2e46faeba100f
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2022-05-19 13:26:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -79,13 +79,12 @@
             </div>
           </div>
       <?php } ?>
-
     </div>
-
   </div>
   
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <script src="js/script.js"></script>
+  
 </body>
 
 </html>
